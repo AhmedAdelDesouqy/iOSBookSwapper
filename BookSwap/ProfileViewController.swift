40abbc8621e9e0501bdddd0692212527d46899c2
Ahmed Hamdy <ahmedhamdy@ahmeds-mac-2.local>
2016-05-24 14:39:55 +0200

    my new edit 24/5 2:39

---
+++ BookSwap/ProfileViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ProfileViewController.swift
+//  BookSwap
+//
+//  Created by Ahmed Hamdy on 5/23/16.
+//  Copyright © 2016 JETS. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController ,UITableViewDelegate{
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.tableView.delegate = self
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- BookSwap/ViewController.swift
+++ BookSwap/ViewController.swift
@@ -8,18 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UICollectionViewDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.collectionView.delegate = self
     }
+    
+    
+    @IBOutlet weak var collectionView: UICollectionView!
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+   
+    
+//    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+//        <#code#>
+//    }
 
 }
 
